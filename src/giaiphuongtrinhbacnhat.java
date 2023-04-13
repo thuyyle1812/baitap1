51f2057c1dea305442a200f821bd87c168f11f2f
thuyyle1812 <114391640+thuyyle1812@users.noreply.github.com>
2023-04-13 01:52:08 -0700

    daybaitap

--- src/giaiphuongtrinhbacnhat.java
+++ src/giaiphuongtrinhbacnhat.java
@@ -8,29 +8,26 @@ public class giaiphuongtrinhbacnhat {
         //Khai bao doi tuong scanner
         Scanner scanner = new Scanner(System.in);
         System.out.print("a:");
-        //nhap gia tri tu
+        //nhap gia tri tu bam phim va gan gia tri vua nhap vao bien a
         double a = scanner.nextDouble();
-         //nhap gia tri tu bam phim va gan gia tri vua nhap vao bien a
         System.out.print("b:");
         // nhap gia tri tu bam phim va gan gia tri vua nhap vao bien b
         double b = scanner.nextDouble();
         //nhap gia tri tu bam phim va gan gia tri vua nhap vao bien c
         System.out.println("c:");
         double c = scanner.nextDouble();
-
         //cau dk
         if (a != 0) {
-            // cau dk neu a khac 0
+            // truong hop a khac 0 thi phuong trinh co 1 nghiem
             double answer = (c - b) / a;
 
             System.out.printf("Equation pass with x = %f!\n", answer);
         } else {
-            //
+            //truong hop b bang c thi phuog trinh vo so nghiem
             if (b == c) {
-
                 System.out.print("The solution is all x!");
             } else {
-
+            //truong hop b khac c thi phuog trinh vo nghiem
                 System.out.print("No solution!");
             }
         }
