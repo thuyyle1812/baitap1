b47946b67a9151e65b853e84633a27a90874f0da
thuyyle1812 <114391640+thuyyle1812@users.noreply.github.com>
2023-04-20 00:46:57 -0700

    daybaitap

--- src/kiemtranamnhuan.java
+++ src/kiemtranamnhuan.java
@@ -1,6 +1,38 @@
+import java.util.Scanner;
+
 public class kiemtranamnhuan {
     public static void main(String[] args) {
-        int nam;
+        // khai báo đối tượng scanner
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhap nam:");
+        //khai bao bien nam va dong thoi nhap gia tri tu ban phim
+        int year = scanner.nextInt();
+        // su dung boolean
+        boolean isLeap = false;
+        // dung if va lay năm % 4 ==0
+        if (year % 4 == 0) {
+            // dung if va lay năm % 100 ==0
+            if (year % 100 == 0) {
+                // dung if va lay năm % 400 ==0
+                if (year % 400 == 0) {
+                    //neu nam nhuan thi chay vao khoi lệnh bên dưới
+                    isLeap = true;
 
+                } else {
+                    //neu nam khong nhuan thi chay vao khoi lệnh bên dưới
+                    isLeap = false;
+                }
+            } else {
+                isLeap = true;
+            }
+        } else {
+            isLeap = false;
+        }
+        if (isLeap) {
+            System.out.println(year + "la năm nhuận");
+
+        } else {
+            System.out.println(year + "khong phai là năm nhuận");
+        }
     }
 }
